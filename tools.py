4499f5ee42a0838aae6a465ebcce1f67fba5a9bb
Juu04up <1711636195@qq.com>
2026-07-15 16:04:19 +0800

    添加了公共工具

---
+++ tools.py
@@ -0,0 +1,81 @@
+import time
+import json
+import logging
+from selenium import webdriver
+from selenium.webdriver.edge.service import Service
+from logging import handlers
+from config import PATH
+
+class DriverTools:
+
+    driver = None
+
+    @classmethod
+    def get_driver(cls):
+
+        if cls.driver is None:
+
+            path = r"D:\Python\msedgedriver.exe"
+            ser = Service(executable_path=path)
+            cls.driver = webdriver.Edge(service=ser)
+
+            cls.driver.maximize_window()
+            cls.driver.implicitly_wait(10)
+        return cls.driver
+
+    @classmethod
+    def quit_driver(cls):
+        if cls.driver:
+            time.sleep(1)
+            cls.driver.quit()
+            cls.driver = None
+
+def read_json(file_name):
+    """
+    读取JSON文件并转换为格式为 [(), (), ...] 的列表
+    :param file_name: json文件名
+    :return: 列表
+    """
+    data = []
+    file_path = PATH + "/data/" + file_name
+    with open(file_path, mode='r', encoding='utf-8') as f:
+        # 读取JSON文件并解析为Python对象
+        tmp = json.load(f)
+        for i in tmp:
+            a = tuple(i.values())
+            data.append(a)
+    # 返回列表
+    return data
+
+class GetLog:
+    # 日志器
+    __log = None
+
+    @classmethod
+    def get_log(cls):
+        if cls.__log is None:
+            # 获取日志器
+            cls.__log = logging.getLogger()
+            # 设置入口级别
+            cls.__log.setLevel(logging.INFO)
+            # 获取处理器
+            filename = PATH + "/log/" + "web.log"
+            tf = logging.handlers.TimedRotatingFileHandler(
+                filename=filename,  # 日志文件名
+                when="midnight",    # 日志归档时间
+                interval=1,         # 每天归档一次
+                backupCount=3,      # 保留3天日志
+                encoding="utf-8"     # 日志编码格式
+            )
+            # 获取格式器
+            fmt = "%(asctime)s %(levelname)s [%(filename)s(%(funcName)s:%(lineno)d)] - %(message)s"
+            fm = logging.Formatter(fmt)
+            # 将格式器添加到处理器
+            tf.setFormatter(fm)
+            # 将处理器添加到日志器
+            cls.__log.addHandler(tf)
+        # 返回日志器
+        return cls.__log
+
+if __name__ == "__main__":
+    print(read_json("login_data.json"))
